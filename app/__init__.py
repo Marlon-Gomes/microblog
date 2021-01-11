0547180efff62f97eee7435ff2264f360d115d05
Marlon-Gomes <marlon.wyd@gmail.com>
2021-01-11 09:45:19 -0500

    Error handling

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+# Log errors by mail
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 # The line below creates an instance,'app', of class Flask, using the predefined
 #__name__ variable, which refers to the name of the module in which it is used.
@@ -24,6 +27,43 @@ migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-# We import routes at the end bottom to avoid circular import. It needs to import
-# the app variable defined in this script.
-from app import routes, models
+# Error logging
+if not app.debug:
+    # Log and mail errors immediately
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (app.config['MAIL_SERVER'],app.config['MAIL_PORT'])
+            fromaddr = 'no-reply' + app.config['MAIL_SERVER'],
+            toaddr = app.config['ADMINS'],
+            subject = 'Microblog Failure',
+            credentials = auth,
+            secure = secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    #
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_hander = RotatingFileHandler(
+        'logs/microblog.log', 
+        maxBytes = 10240,
+        backupCount = 10)
+    file_hander.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s \
+            [in %(pathname)s: %(lineno)d]'))
+    file_hander.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_hander)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+# We import 'routes', 'models', 'errors' at the end bottom to avoid circular
+# import as they need to import 'app', 'db', 'login' defined on this script.
+from app import routes, models, errors
